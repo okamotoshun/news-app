e6e37d697ffddeeec763ee7185a342901d9a029c
shun-okamoto <okamons8811@gmail.com>
2023-02-14 22:27:12 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+          // port: '',
+          // pathname: '/account123/**',
+        },
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
